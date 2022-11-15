19177f9c4ab90afd28291c522ee0674c3a771a40
haroldadrada <andresadrada@gmail.com>
2022-11-15 11:36:45 -0500

    MAnejo de excepciones: raise, try, except y finally. Ejemplo usa try y except

---
+++ 06_manejo_excepciones.py
@@ -0,0 +1,23 @@
+"""
+Manejo de excepciones: raise, try, except y finally
+"""
+
+def divisors(num):
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+
+def run():
+    try:
+        num = int(input("Ingrese un número: "))
+        print(divisors(num))
+        print("Terminó mi programa")
+    except ValueError:
+        print("Debes ingresar un número")
+
+
+if __name__ == '__main__':
+    run()
