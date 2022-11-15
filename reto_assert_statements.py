269f54da9eb1b640507075c2c1f240e84deb533b
haroldadrada <andresadrada@gmail.com>
2022-11-15 15:16:13 -0500

    Reto de assert statements

---
+++ reto_assert_statements.py
@@ -0,0 +1,22 @@
+"""
+Assert statements
+"""
+
+
+def divisors(num):
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+
+def run():
+    num = input("Ingrese un número: ")
+    assert num.isnumeric() and int(num)>0, "Debes ingresar solo numeros positivos"
+    print(divisors(int(num)))
+    print("Terminó mi programa")
+
+
+if __name__ == '__main__':
+    run()
